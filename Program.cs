dbfd9cf644fc69a263cb172fa69c511b5ca02f08
MukheshRavi <mukeshparvathi@gmail.com>
2020-09-26 13:37:04 +0530

    uc1 added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+            Random r = new Random();
+
+            i = r.Next(2);
+            if (i == 0)
+                Console.WriteLine("employee absent ");
+            else
+                Console.WriteLine("employee present");
+        }
+    }
+    }
+
