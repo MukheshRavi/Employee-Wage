941e7bc60f3451a234c88c5853fcacf734348be4
MukheshRavi <mukeshparvathi@gmail.com>
2020-09-27 21:49:36 +0530

    uc3 added

--- Program.cs
+++ Program.cs
@@ -11,14 +11,21 @@ namespace EmployeeWageProblem
                 Console.WriteLine("Employee is Absent!");
             else
                 Console.WriteLine("Employee is Present!");
-            int dailyEmployeeWage = 0;
+            int dailyEmployeeWage = 0, partTimeWage = 0;
             if (isPresent == 1)
             {
                 dailyEmployeeWage = DailyEmployeeWage();
+                int doPartTime = Attendance();
+                if (doPartTime == 1)
+                    partTimeWage = PartTimeWage();
             }
             Console.WriteLine("Daily Employee Wage : {0}", dailyEmployeeWage);
+            Console.WriteLine("Part Time Employee Wage : {0}", partTimeWage);
         }
-
+        /// <summary>
+        /// Attendance function returns 0 or 1 randomly.
+        /// </summary>
+        /// <returns></returns>
         static int Attendance()
         {
             Random randObj = new Random();
@@ -30,6 +37,13 @@ namespace EmployeeWageProblem
             int fullDayHour = 8;
             int perHourWage = 20;
             return fullDayHour * perHourWage;
+        }
+
+        static int PartTimeWage()
+        {
+            int partTimeHour = 4;
+            int perHourWage = 20;
+            return partTimeHour * perHourWage;
         }
     }
 }
