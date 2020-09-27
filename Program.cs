8b98e39d9181952ff04e18414db95015ccca170a
MukheshRavi <mukeshparvathi@gmail.com>
2020-09-27 20:40:26 +0530

    uc2 added

--- Program.cs
+++ Program.cs
@@ -7,15 +7,32 @@ namespace Uc1
         static void Main(string[] args)
         {
 
-            int i;
-            Random r = new Random();
-
-            i = r.Next(2);
-            if (i == 0)
-                Console.WriteLine("employee absent ");
+            int isPresent = Attendance();
+            if (isPresent == 0)
+                Console.WriteLine("Employee is Absent!");
             else
-                Console.WriteLine("employee present");
+                Console.WriteLine("Employee is Present!");
+            int dailyEmployeeWage = 0;
+            if (isPresent == 1)
+            {
+                dailyEmployeeWage = DailyEmployeeWage();
+            }
+            Console.WriteLine("Daily Employee Wage : {0}", dailyEmployeeWage);
+        }
+
+        static int Attendance()
+        {
+            Random randObj = new Random();
+            return randObj.Next(0, 2);
+        }
+
+        static int DailyEmployeeWage()
+        {
+            int fullDayHour = 8;
+            int perHourWage = 20;
+            return fullDayHour * perHourWage;
         }
     }
-    }
+}
+    
 
