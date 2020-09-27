7f7f98cd189b6c8628a6e42e6db3b82c69692522
MukheshRavi <mukeshparvathi@gmail.com>
2020-09-27 21:07:26 +0530

    master added

--- Program.cs
+++ Program.cs
@@ -7,32 +7,8 @@ namespace Uc1
         static void Main(string[] args)
         {
 
-            int isPresent = Attendance();
-            if (isPresent == 0)
-                Console.WriteLine("Employee is Absent!");
-            else
-                Console.WriteLine("Employee is Present!");
-            int dailyEmployeeWage = 0;
-            if (isPresent == 1)
-            {
-                dailyEmployeeWage = DailyEmployeeWage();
-            }
-            Console.WriteLine("Daily Employee Wage : {0}", dailyEmployeeWage);
-        }
-
-        static int Attendance()
-        {
-            Random randObj = new Random();
-            return randObj.Next(0, 2);
-        }
-
-        static int DailyEmployeeWage()
-        {
-            int fullDayHour = 8;
-            int perHourWage = 20;
-            return fullDayHour * perHourWage;
+            Console.WriteLine("welcome to employee wage computation problem ");
         }
     }
 }
-    
 
